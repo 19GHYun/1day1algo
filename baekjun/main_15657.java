068cfbd3811468fb98f6069821e990eda3f2fb25
GyeongHo Yun <94778099+19GHYun@users.noreply.github.com>
2025-01-29 23:03:58 +0900

    Create main_15657.java

---
+++ baekjun/main_15657.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] ans;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	N = sc.nextInt();
+    	M = sc.nextInt();
+
+        arr = new int[N];
+        ans = new int[M];
+
+        for(int i = 0 ; i < N ; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        dfs(0, 0);
+    }
+    public static void dfs(int depth, int index){
+        if (depth == M){
+            for (int x : ans){
+                System.out.printf("%d ", x);
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = index; i < N; i++){
+            ans[depth] = arr[i];
+            dfs(depth + 1, i);
+        }
+    }
+}
