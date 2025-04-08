7e6833323b780d7ce53d4423a63a388177c502b4
GyeongHo Yun <94778099+19GHYun@users.noreply.github.com>
2025-04-09 08:46:44 +0900

    Create main_1818.java

---
+++ baekjun/main_1818.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(arr[0]);
+		
+		for(int i = 1 ; i < N ; i++) {
+			if(arr[i] > list.get(list.size()-1)) {
+				list.add(arr[i]);
+			}
+			else {
+				int index = bs(list, arr[i]);
+				list.set(index, arr[i]);
+			}
+		}
+		
+		System.out.println(N - list.size());
+		
+		
+	}
+	
+	public static int bs(ArrayList<Integer> list, int num) {
+		int left = 0;
+		int right = list.size();
+		
+		while(left < right) {
+			int mid = (left + right) / 2;
+			
+			if(list.get(mid) < num) {
+				left = mid + 1;
+			}
+			else {
+				right = mid;
+			}
+			
+		}
+		
+		return left;
+	}
+
+	
+}
