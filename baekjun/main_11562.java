8393fb73366191712cad7fad7107d4cb095aee5e
GyeongHo Yun <94778099+19GHYun@users.noreply.github.com>
2025-04-10 09:03:25 +0900

    Create main_11562.java

---
+++ baekjun/main_11562.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[V+1][V+1];
+		
+		for(int i = 1 ; i <= V ; i++) {
+			Arrays.fill(arr[i], Integer.MAX_VALUE);
+			arr[i][i] = 0;
+		}
+		
+		
+		for(int i = 0 ; i < E ; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a1 = Integer.parseInt(st.nextToken());
+			int a2 = Integer.parseInt(st.nextToken());
+			int a3 = Integer.parseInt(st.nextToken());
+			
+			if(a3 == 0) {
+				arr[a1][a2] = 0;
+				arr[a2][a1] = 1;
+			}
+			else {
+				arr[a1][a2] = 0;
+				arr[a2][a1] = 0;
+			}
+
+		}
+		
+		for(int i = 1 ; i <= V ; i++) {
+			for(int ii = 1 ; ii <= V ; ii++) {
+				for(int iii = 1 ; iii <= V ; iii++) {
+					if(arr[ii][i] != Integer.MAX_VALUE && arr[i][iii] != Integer.MAX_VALUE) {
+						arr[ii][iii] = Math.min(arr[ii][iii], arr[ii][i] + arr[i][iii]);
+					}
+					
+				}
+			}
+		}
+		
+//		for(int i = 1 ; i <= V ; i++) {
+//			for(int ii = 1 ; ii <= V ; ii++) {
+//				System.out.print(arr[i][ii] + " ");
+//			}
+//			System.out.println();
+//		}
+		
+		int K = Integer.parseInt(br.readLine());
+		
+		for(int i = 0 ; i < K ; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a1 = Integer.parseInt(st.nextToken());
+			int a2 = Integer.parseInt(st.nextToken());
+
+			sb.append(arr[a1][a2]).append("\n");
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+	
+
+	
+}
